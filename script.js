d2e6998cdd8f3b0be4d513210e67354abdcf98aa
Maria Alfonso <mariapaulaalfonso@gmail.com>
2023-03-25 18:45:38 -0500

    Dinamyc button to disaggregate cards from favorite, in addition to improved scalability in the kitten cards creation function.

--- script.js
+++ script.js
@@ -3,13 +3,22 @@ const API_URL_RANDOM =
 const API_URL_FAVORITES =
   "https://api.thecatapi.com/v1/favourites?api_key=live_iZ6Eefeyg5DLUA6jbq3Hpur4QJE4fF3mIT6zQ6Q0glEARel7jl6LqfMmbcCdpVsg";
 
+const apiUrlFavoriteDelete = (id) =>
+  `https://api.thecatapi.com/v1/favourites/${id}?api_key=live_iZ6Eefeyg5DLUA6jbq3Hpur4QJE4fF3mIT6zQ6Q0glEARel7jl6LqfMmbcCdpVsg`;
+
 const btn = document.querySelector(".reload-kittens-images");
 const spanError = document.getElementById("error");
 const starIcon = document.querySelectorAll(".star-icon");
-// hola
+
 btn.addEventListener("click", loadRandomKittens);
 
-function createKittyCard(kitty, iconUrl, alt) {
+function createKittyCard({
+  kitty,
+  iconUrl,
+  classIconStar,
+  altImgKitty,
+  onClick,
+}) {
   const cardContainer = document.createElement("div");
   const figureIcon = document.createElement("figure");
   const imgIcon = document.createElement("img");
@@ -17,14 +26,16 @@ function createKittyCard(kitty, iconUrl, alt) {
 
   cardContainer.setAttribute("class", "img-container");
   figureIcon.setAttribute("class", "icon-container");
-  imgIcon.setAttribute("class", "star-icon");
+  imgIcon.setAttribute("class", classIconStar);
   imgIcon.setAttribute("src", iconUrl);
-  imgIcon.setAttribute("alt", "star");
+  imgIcon.setAttribute("alt", "icon-star");
   imgKitty.setAttribute("id", kitty.id);
-  imgKitty.setAttribute("alt", alt);
+  imgKitty.setAttribute("alt", altImgKitty);
   imgKitty.setAttribute("src", kitty.url);
 
-  imgIcon.addEventListener("click", () => saveFavoriteKitty(kitty.id));
+  if (onClick) {
+    imgIcon.addEventListener("click", onClick);
+  }
 
   const fragment = document.createDocumentFragment();
   figureIcon.appendChild(imgIcon);
@@ -37,9 +48,6 @@ function createKittyCard(kitty, iconUrl, alt) {
 const kittyContainer = document.getElementById("random-kittens-container");
 
 async function loadRandomKittens() {
-  // Vaciar el contenedor antes de agregar nuevos elementos
-  kittyContainer.innerHTML = "";
-
   const response = await fetch(API_URL_RANDOM);
   const data = await response.json();
 
@@ -51,21 +59,25 @@ async function loadRandomKittens() {
   } else {
     const fragment = document.createDocumentFragment();
     data.forEach((item) => {
-      const kittyElement = createKittyCard(
-        item,
-        "/others/star.png",
-        "Random Kitty"
-      );
+      const kittyElement = createKittyCard({
+        kitty: item,
+        iconUrl: "/others/star.png",
+        classIconStar: "star-icon",
+        altImgKitty: "Random Kitty",
+        onClick: () => saveFavoriteKitty(item.id),
+      });
       fragment.appendChild(kittyElement);
     });
 
-    kittyContainer.appendChild(fragment);
+    kittyContainer.replaceChildren(fragment);
   }
 }
 
-// saveFavoriteKitty(data[0].id);   -> El objeto de michis random y favoritos es un poco diferente.
-
 async function loadFavoritesKittens() {
+  const favoriteKittyContainer = document.getElementById(
+    "favorites-kittens-container"
+  );
+
   const response = await fetch(API_URL_FAVORITES);
   const data = await response.json();
 
@@ -73,38 +85,31 @@ async function loadFavoritesKittens() {
   console.log(data);
 
   if (response.status !== 200) {
-    console.log(response);
     spanError.innerText = `Error ${response.status}. Try later.`;
-
-    // El array de michis favoritos tiene objetos y cada objeto tiene el image_id y una propiedad imd con el mismo id y una url.
-    // recorrer el array de data para obtener la url y ponersela a las imagenes de michis favoritos.
   } else {
     const fragment = document.createDocumentFragment();
-    const favoriteKittyContainer = document.getElementById(
-      "favorites-kittens-container"
-    );
 
     data.forEach((favoriteKitty) => {
-      const kittyElement = createKittyCard(
-        favoriteKitty.image,
-        "/others/star-remove.png",
-        "Favorite Kitty Pic"
-      );
+      const kittyElement = createKittyCard({
+        kitty: favoriteKitty.image,
+        iconUrl: "/others/star-remove.png",
+        classIconStar: "remove-star-icon",
+        altImgKitty: "Favorite Kitty Pic",
+        onClick: () => deleteFavoriteKitty(favoriteKitty.id),
+      });
       fragment.appendChild(kittyElement);
     });
-    favoriteKittyContainer.appendChild(fragment);
+    favoriteKittyContainer.replaceChildren(fragment);
   }
 }
 
 async function saveFavoriteKitty(id) {
-  // console.log(id);
   const response = await fetch(API_URL_FAVORITES, {
     method: "POST",
     headers: {
-      "Content-Type": "application/json", // -> Enviar un JSON que es el lenguaje comun entre frontend y backend
+      "Content-Type": "application/json",
     },
     body: JSON.stringify({
-      // -> El JSON.stringify convierte un objeto en una cadena de texto JSON.
       image_id: id,
     }),
   });
@@ -115,9 +120,27 @@ async function saveFavoriteKitty(id) {
 
   if (response.status !== 200) {
     spanError.innerText = `Error ${response.status}. Try later.`;
+  } else {
+    console.log("Kitty saved in favorites");
   }
   loadFavoritesKittens();
 }
 
-loadRandomKittens(); // -> Hacer llamado a la funcion apenas cargue el codigo js para que cuando entremos a la pagina evitemos que no aparezca vacia.
+async function deleteFavoriteKitty(id) {
+  const response = await fetch(apiUrlFavoriteDelete(id), {
+    method: "DELETE",
+  });
+  const data = await response.json();
+  console.log("Deleted");
+  console.log(data);
+
+  if (response.status !== 200) {
+    spanError.innerText = `Error ${response.status}. Try later.`;
+  } else {
+    console.log("Kitty removed from favorites");
+  }
+  loadFavoritesKittens();
+}
+
+loadRandomKittens();
 loadFavoritesKittens();
